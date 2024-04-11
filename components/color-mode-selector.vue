c82a4308d3f5f1a6958727569373aff8d2cc2b05
Daniel Shahzada <107118950+PS213073@users.noreply.github.com>
2024-04-11 20:37:39 +0200

    dark mode selector button

---
+++ components/color-mode-selector.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="flex space-x-2 items-center">
+    <div class="text-gray-500 text-xs" v-if="showNextModeLabel">Change to {{ nextMode }}</div>
+    <button
+      @click="toggleMode" @mouseenter="showNextModeLabel = true" @mouseleave="showNextModeLabel= false"
+      class="hover:bg-gray-200 dark:hover:bg-gray-600 px-2 py-1 text-gray-500"
+    >
+      {{ nextModeIcon }}
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+const showNextModeLabel = ref(false);
+const colorMode = useColorMode();
+
+const modes = [
+  "system", //0
+  "light", //1
+  "dark", //2
+]; // .length = 3
+
+const nextModeIcons: Record<string, string> = {
+  system: "🌓",
+  light: "🌕",
+  dark: "🌑",
+};
+
+const nextMode = computed(() => {
+  const currentModeIndex = modes.indexOf(colorMode.preference);
+  let nextModeIndex = null;
+
+  if (currentModeIndex + 1 === modes.length) {
+    nextModeIndex = 0;
+  } else {
+    nextModeIndex = currentModeIndex + 1;
+  }
+
+  return modes[nextModeIndex];
+});
+
+const nextModeIcon = computed(() => nextModeIcons[nextMode.value]);
+
+const toggleMode = () => (colorMode.preference = nextMode.value);
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -3,12 +3,18 @@ import { NuxtLink } from '#build/components';
 <template>
   <div class="container mx-auto max-w-2xl">
     <header class="flex justify-between items-center mt-5">
-      <div>
-        <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200"
-          >Daniel Shahzada</NuxtLink
-        >
+      <div class="flex items-center space-x-12">
+        <div>
+          <NuxtLink to="/" class="text-xl font-semibold p-2 hover:bg-gray-200"
+            >Daniel Shahzada</NuxtLink
+          >
+        </div>
+        <Menu />
       </div>
-      <Menu />
+
+      <ClientOnly>
+        <ColorModeSelector/>
+      </ClientOnly>
     </header>
     <main class="p-2 mt-10">
       <slot />
@@ -17,9 +23,9 @@ import { NuxtLink } from '#build/components';
 </template>
 
 <script setup>
-const colorMode = useColorMode()
-console.log(colorMode.preference)
-colorMode.preference = 'light'
+// const colorMode = useColorMode();
+// console.log(colorMode.preference);
+// colorMode.preference = "light";
 // colorMode.preference = 'dark'
 
 useHead({
@@ -39,8 +45,8 @@ useHead({
 </script>
 
 <style>
-body{
+body {
   /* font-family: Roboto; */
-  @apply bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-300
+  @apply bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-300;
 }
 </style>
