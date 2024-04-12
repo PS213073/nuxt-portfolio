c681ea4da3974873b4a98d53570ddb58cfee4043
Daniel Shahzada <107118950+PS213073@users.noreply.github.com>
2024-04-12 16:13:54 +0200

    vue components inside markdown file || MDC syntax!

---
+++ components/content/blog-posts.vue
@@ -0,0 +1,18 @@
+<template>
+    <section class="not-prose">
+      <ul>
+        <li v-for="post in posts" :key="post._path">
+        <NuxtLink :to="post._path">{{ post.title }}</NuxtLink>
+        </li>
+      </ul>
+    </section>
+  </template>
+  
+  <script setup>
+  const { data: posts } = await useAsyncData(
+    'blog-list', () => queryContent('/blog')
+    .where({_path: {$ne: '/blog'}})
+    .only(['title', '_path'])
+    .find())
+  console.log(posts);
+  </script>
---
+++ content/blog/index.md
@@ -0,0 +1,9 @@
+---
+description: 'List of all my recent blog posts'
+---
+
+# Blog Post List
+
+This is the list of my recent blog posts
+
+::blog-posts
