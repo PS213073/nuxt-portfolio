8a6742ab9ffdb467a81eb593b0e134f4b481772d
Daniel Shahzada <107118950+PS213073@users.noreply.github.com>
2024-04-13 00:10:25 +0200

    error handeling 404 page not found

---
+++ error.vue
@@ -0,0 +1,23 @@
+
+import { NuxtLayout } from '#build/components';
+<template>
+  <NuxtLayout>
+    <section>
+      <h1 class="text-4xl mb-4">Opps {{ error.statusCode }}</h1>
+      <h2 class="text-xl text-gray-500 dark:text-gray-400">
+        {{ error.statusMessage }}
+      </h2>
+    </section>
+  </NuxtLayout>
+</template>
+
+
+
+<script setup>
+defineProps({
+  error: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -2,36 +2,42 @@
   <article
     class="prose dark:prose-invert prose-h1:font-semibold prose-pre:bg-white dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300 max-w-none"
   >
-    <ContentDoc v-slot="{ doc }">
-      <div class="grid grid-cols-6 gap-20">
-        <div
-          :class="{
-            'col-span-6 md:col-span-4': doc.toc,
-            'col-span-6': !doc.toc,
-          }"
-        >
-          <ContentRenderer :value="doc" />
+    <ContentDoc>
+      <template #not-found>
+        <h1>Document not found (404)</h1>
+        <p>This blog post could not be found</p>
+      </template>
+      <template v-slot="{ doc }">
+        <div class="grid grid-cols-6 gap-20">
+          <div
+            :class="{
+              'col-span-6 md:col-span-4': doc.toc,
+              'col-span-6': !doc.toc,
+            }"
+          >
+            <ContentRenderer :value="doc" />
+          </div>
+          <div class="hidden md:col-span-2 md:block not-prose" v-if="doc.toc">
+            <aside class="sticky top-8">
+              <div class="font-semibold mb-2">Table of Contents</div>
+              <nav>
+                <TocLinks :links="doc.body.toc.links" :active-id="activeId" />
+              </nav>
+            </aside>
+          </div>
         </div>
-        <div class="hidden md:col-span-2 md:block not-prose" v-if="doc.toc">
-          <aside class="sticky top-8">
-            <div class="font-semibold mb-2">Table of Contents</div>
-            <nav>
-              <TocLinks :links="doc.body.toc.links" :active-id="activeId" />
-            </nav>
-          </aside>
-        </div>
-      </div>
+      </template>
     </ContentDoc>
   </article>
 </template>
 
 <script setup>
-const activeId = ref(null)
+const activeId = ref(null);
 onMounted(() => {
   const callback = (entries) => {
-    for (const entry of entries){
-      if(entry.isIntersecting){
-        activeId.value = entry.target.id
+    for (const entry of entries) {
+      if (entry.isIntersecting) {
+        activeId.value = entry.target.id;
         break;
       }
     }
@@ -40,18 +46,18 @@ onMounted(() => {
   const observer = new IntersectionObserver(callback, {
     root: null,
     threshold: 0.5,
-  })
-  const elements = document.querySelectorAll('h2', 'h3')
+  });
+  const elements = document.querySelectorAll("h2", "h3");
 
-  for (const element of elements){
-    observer.observe(element)
+  for (const element of elements) {
+    observer.observe(element);
   }
 
   onBeforeUnmount(() => {
     // console.log('onBeforeUnmount');
-    for (const elemet of elements){
-      observer.unobserve(elemet)
+    for (const elemet of elements) {
+      observer.unobserve(elemet);
     }
-  })
-})
+  });
+});
 </script>
