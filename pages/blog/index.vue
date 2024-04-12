61769f2fb1a163736db6fc07f4e350378e100bd9
Daniel Shahzada <107118950+PS213073@users.noreply.github.com>
2024-04-12 15:46:20 +0200

    blog post list - quering content

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,3 +1,14 @@
 <template>
-  <div>All blog post list!</div>
+  <section>
+    <ul>
+      <li v-for="post in posts" :key="post._path">
+      <NuxtLink :to="post._path">{{ post.title }}</NuxtLink>
+      </li>
+    </ul>
+  </section>
 </template>
+
+<script setup>
+const { data: posts } = await useAsyncData('blog-list', () => queryContent('/blog').only(['title', '_path']).find())
+console.log(posts);
+</script>
